a15f1db4f0e47229c6ea69ca329cd0b2733d4ec0
Ebenezer Don <ebenezerdonu@gmail.com>
2021-05-20 08:35:16 +0100

    fix typo error

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,6 +8,6 @@ app.get('/', (req, res) => res.status(200).send('Hey there!'))
 app.post('/entries', addEntry)
 app.get('/entries', getAllEntries)
 app.patch('/entries/:entryId', updateEntry)
-app.delete('/entries/:enrryId', deleteEntry)
+app.delete('/entries/:entryId', deleteEntry)
 
 exports.app = functions.https.onRequest(app)
